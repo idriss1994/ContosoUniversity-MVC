6a136702c1c208172498df38578fbefdca23e87c
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-13 00:52:01 +0100

    Adding SchoolContext

---
+++ ContosoUniversity MVC/Data/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using ContosoUniversity_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity_MVC.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable(nameof(Course));
+            modelBuilder.Entity<Enrollment>().ToTable(nameof(Enrollment));
+            modelBuilder.Entity<Student>().ToTable(nameof(Student));
+        }
+    }
+}
