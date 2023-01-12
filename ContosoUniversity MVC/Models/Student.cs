517cc407b193c5bbde92ffe4d4ddd317d7626073
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-12 22:14:01 +0100

    Adding Course, Enrollment and Student models

---
+++ ContosoUniversity MVC/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ContosoUniversity_MVC.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
