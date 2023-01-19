ed6e46da7363da18c88fa42b9366951be7b8b01d
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-19 11:28:27 +0100

    Implement inheritance, table-per-hierarchy (TPH) inheritance.

--- ContosoUniversity MVC/Models/Instructor.cs
+++ ContosoUniversity MVC/Models/Instructor.cs
@@ -7,34 +7,14 @@ using System.Xml.Linq;
 
 namespace ContosoUniversity_MVC.Models
 {
-    public class Instructor
+    public class Instructor : Person
     {
-        public int ID { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
-        [StringLength(50)]
-        public string LastName { get; set; }
-
-        [Required]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        [StringLength(50)]
-        public string FirstMidName { get; set; }
-
         [DataType(DataType.Date)]
         [Display(Name = "Hire Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime HireDate { get; set; }
 
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get { return LastName + ", " + FirstMidName; }
-        }
-
         public ICollection<CourseAssignment> CourseAssignments { get; set; }
         public OfficeAssignment OfficeAssignment { get; set; }
-
     }
 }
