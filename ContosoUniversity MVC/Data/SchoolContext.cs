bc0b5f9f6cb3383fff7a360a2f7f2176f9ae9246
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-19 11:29:31 +0100

    Mapping Student and Instructor Entities to Person table

--- ContosoUniversity MVC/Data/SchoolContext.cs
+++ ContosoUniversity MVC/Data/SchoolContext.cs
@@ -16,17 +16,19 @@ namespace ContosoUniversity_MVC.Data
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
         public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable(nameof(Course));
             modelBuilder.Entity<Enrollment>().ToTable(nameof(Enrollment));
-            modelBuilder.Entity<Student>().ToTable(nameof(Student));
+            modelBuilder.Entity<Student>().ToTable(nameof(Person));
             modelBuilder.Entity<Department>().ToTable(nameof(Department));
-            modelBuilder.Entity<Instructor>().ToTable(nameof(Instructor));
+            modelBuilder.Entity<Instructor>().ToTable(nameof(Person));
             modelBuilder.Entity<OfficeAssignment>().ToTable(nameof(OfficeAssignment));
             modelBuilder.Entity<CourseAssignment>().ToTable(nameof(CourseAssignment));
-
+            modelBuilder.Entity<Person>().ToTable(nameof(Person));
+               
             modelBuilder.Entity<CourseAssignment>()
                 .HasKey(c => new { c.CourseID, c.InstructorID });
         }
